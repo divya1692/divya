0d13fc11d0b6d7ba00380bc895ec6eeef04b4dc3
unknown <ac.divya@gmail.com>
2017-08-06 22:51:05 +0530

    upload

---
+++ src/utility/Screenshot.java
@@ -0,0 +1,35 @@
+package utility;
+
+import org.openqa.selenium.TakesScreenshot;
+import java.io.File;
+import java.io.IOException; 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver; 
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Screenshot {
+	
+	public static void captureScreenShot(WebDriver ldriver){
+		 
+		  // Take screenshot and store as a file format
+		  File src= ((TakesScreenshot)ldriver).getScreenshotAs(OutputType.FILE);
+		try {
+		  // now copy the  screenshot to desired location using copyFile method
+		 
+		 FileUtils.copyFile(src, new File("C:/selenium/"+System.currentTimeMillis()+".png"));
+		       }
+		 
+		catch (IOException e)
+		 
+		{
+		 
+		System.out.println(e.getMessage());
+		 
+		    }
+		 
+		}
+
+}
